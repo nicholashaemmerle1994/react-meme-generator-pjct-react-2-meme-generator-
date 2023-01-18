c790baae48aaa509f448bb52ce84b61f7cdac395
nicholashaemmerle1994 <nicholashaemmerle@gmx.at>
2023-01-18 15:59:39 +0100

    Done the logic

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import { saveAs } from 'file-saver';
+import { useState } from 'react';
+
+const App = () => {
+  const [memeTemplate, setMemeTemplate] = useState('buzz');
+  const [topText, setTopText] = useState(' ');
+  const [bottomText, setBottomText] = useState(' ');
+
+  return (
+    <div>
+      <h1>The Meme Generator</h1>
+      <label>
+        Meme template
+        <input
+          onChange={(event) => {
+            setMemeTemplate(event.currentTarget.value);
+          }}
+          className="memeTemplate"
+          placeholder="Meme Template"
+        />
+      </label>
+      <br />
+      <br />
+      <label htmlFor="topText">
+        Top text
+        <input
+          onChange={(event) => {
+            setTopText(event.currentTarget.value);
+          }}
+          className="topText"
+          placeholder="Top Text"
+        />
+      </label>
+      <br />
+      <br />
+      <label htmlFor="bottomText">
+        Bottom text
+        <input
+          onChange={(event) => {
+            setBottomText(event.currentTarget.value);
+          }}
+          className="bottomText"
+          placeholder="Bottom Text"
+        />
+      </label>
+      <br />
+      <br />
+      <br />
+      <img
+        src={`https://api.memegen.link/images/${memeTemplate}/${topText}/${bottomText}`}
+        alt="meme"
+        data-test-id="meme-image"
+      />
+
+      <button
+        onClick={() => {
+          saveAs(
+            `https://api.memegen.link/images/${memeTemplate}/${topText}/${bottomText}`,
+            `${memeTemplate}.jpg`,
+          );
+        }}
+      >
+        Download
+      </button>
+    </div>
+  );
+};
+
+export default App;
